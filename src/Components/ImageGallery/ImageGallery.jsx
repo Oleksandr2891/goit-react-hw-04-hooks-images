3bef46b1c982defcd812cf54d86a5c8dd3ad8555
Oleksandr2891 <allavrenko@gmail.com>
2021-08-31 09:53:57 +0300

    task done

--- src/Components/ImageGallery/ImageGallery.jsx
+++ src/Components/ImageGallery/ImageGallery.jsx
@@ -1,89 +1,104 @@
-import { useState, useEffect } from 'react';
-import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
-import { toast } from 'react-toastify';
-import Api from '../../utils/Api'
-import Button from '../Button/Button';
-import Modal from '../Modal/Modal';
+import { useState, useEffect } from "react";
+import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
+import { toast } from "react-toastify";
+import Api from "../../utils/Api";
+import Button from "../Button/Button";
+import Modal from "../Modal/Modal";
 import LoaderSpiner from "../Loader/Loader";
-import { ImageGalleryList } from './ImageGalleryStyled';
-import 'react-toastify/dist/ReactToastify.css';
+import { ImageGalleryList } from "./ImageGalleryStyled";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function ImageGallery({ nameSearch }) {
     const [page, setPage] = useState(1);
     const [imageData, setImageData] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [onLoadImage, setOnLoadImage] = useState(false);
-    const [imagePath, setImagePath] = useState('');
-    const [imageNameSearch, setImageNameSearch] = useState("");
-    const notifySuccess = () => toast.success('Поиск успешно выполнен!!!');
-    const notifyError = () => toast.error('По вашему запросу ничего не найдено');
-
-    useEffect(() => {
-        if (nameSearch !== '') {
-            setImageNameSearch(nameSearch)
-            setPage(1)
-            getListImageGallery(imageNameSearch, page)
-        } else { return false }
-    }, [nameSearch]);
-
-    useEffect(() => {
-        if (page === 1) return false
-        window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
-        });
-    }, [imageData])// eslint-disable-line
-    const getListImageGallery = async (imageNameSearch, page) => {
+    const [imagePath, setImagePath] = useState("");
+    const notifySuccess = () => toast.success("Поиск успешно выполнен!!!");
+    const notifyError = () => toast.error("По вашему запросу ничего не найдено");
+    const getListImageGallery = async (nameSearch, page) => {
         setIsModalOpen(true);
         setOnLoadImage(true);
-        console.log(imageNameSearch);
         try {
-            const response = await Api(imageNameSearch, page);
+            const response = await Api(nameSearch, page);
             if (response.length) {
-                setImageData(prevImageData => prevImageData ? [...prevImageData, ...response] : response)
-                notifySuccess();
+                setImageData((prevImageData) =>
+                    prevImageData ? [...prevImageData, ...response] : response
+                );
+                (nameSearch !== '') && notifySuccess();
             } else {
                 return notifyError();
             }
         } catch (error) {
             alert(error);
         } finally {
-            page !== 1 ?? window.scrollTo({
-                top: document.documentElement.scrollHeight,
-                behavior: 'smooth',
-            });
             setIsModalOpen(false);
             setOnLoadImage(false);
+            page !== 1 && window.scrollTo({
+                top: document.documentElement.scrollHeight,
+                behavior: "smooth",
+            });
+
         }
-    }
+    };
+
+    useEffect(() => {
+        if (nameSearch !== "") {
+            setImageData([])
+            getListImageGallery(nameSearch, page);
+        } else {
+            return false;
+        }
+    }, [nameSearch]); // eslint-disable-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        if (page !== 1) getListImageGallery(nameSearch, page);
+    }, [page]); // eslint-disable-line react-hooks/exhaustive-deps
+
     const closeModal = () => {
         setIsModalOpen(false);
-        setImagePath('');
+        setImagePath("");
     };
     const openModalWithImage = (path) => {
         setIsModalOpen(true);
         setImagePath(path);
-    }
-    const onHandleLoadMore = () => setPage(prevPage => prevPage + 1)
+    };
+    const onHandleLoadMore = () => setPage((prevPage) => prevPage + 1);
     return (
         <>
             <ImageGalleryList>
-                {imageData && imageData.map(({ id, webformatURL, largeImageURL, tag }) => {
-                    return < ImageGalleryItem
-                        key={id}
-                        id={id}
-                        tag={tag}
-                        webformatURL={webformatURL}
-                        largeImageURL={largeImageURL}
-                        openModalWithImage={openModalWithImage} />
-                })}
+                {imageData &&
+                    imageData.map(({ id, webformatURL, largeImageURL, tag }) => {
+                        return (
+                            <ImageGalleryItem
+                                key={id}
+                                id={id}
+                                tag={tag}
+                                webformatURL={webformatURL}
+                                largeImageURL={largeImageURL}
+                                openModalWithImage={openModalWithImage}
+                            />
+                        );
+                    })}
             </ImageGalleryList>
-            {imageData?.length && <Button onHandleLoadMore={onHandleLoadMore} />}
+            {!!imageData?.length && <Button onHandleLoadMore={onHandleLoadMore} />}
             {isModalOpen && (
                 <Modal closeModal={closeModal}>
-                    {onLoadImage ? <LoaderSpiner /> : <img src={imagePath} width="1000" height="800" alt="ItisPhoto" className="imageInModal" />}
+                    {onLoadImage ? (
+                        <LoaderSpiner />
+                    ) : (
+                        <img
+                            src={imagePath}
+                            width="1000"
+                            height="800"
+                            alt="ItisPhoto"
+                            className="imageInModal"
+                        />
+                    )}
                 </Modal>
             )}
         </>
     );
 }
+
+
+
